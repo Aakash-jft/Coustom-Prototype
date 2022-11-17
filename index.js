afe3324c6968ed4c789d7a0882e946f8af3ddd8d
Aakash-jft <aakash.yadav@jellyfishtechnologies.com>
2022-11-17 16:33:12 +0530

    Made a fillNum prototype for Array

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ let arr = [1,2,3,4];
 Array.prototype.add = function(){
     let sum=0;
     for(let i=0;i<this.length;i++){
-        sum+=this[i]
+        if(typeof(this[i]=="number")){
+            sum+=this[i]
+        }
+        
     }
     return sum;
 }
@@ -22,3 +25,18 @@ Array.prototype.break = function(start,end){
 }
 
 console.log(arr.break(1,3))
+
+Array.prototype.fillNum = function (){
+    let arr=[];
+
+    for(let i=0;i<this.length;i++){
+        if(typeof(this[i])!="number"){
+            continue;
+        }
+        arr.push(this[i])
+    }
+    return arr;
+
+}
+
+console.log(arr.fillNum())
