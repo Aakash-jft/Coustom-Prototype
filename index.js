c21fef27d6a5cfafdb98434f1ee662e5c6a20dc2
Aakash-jft <aakash.yadav@jellyfishtechnologies.com>
2022-11-18 10:24:13 +0530

    added  comments

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-let arr = [1,2,3,4];
+let arr = [1, 2, 3, 4];
 
-Array.prototype.add = function(){
-    let sum=0;
-    for(let i=0;i<this.length;i++){
-        if(typeof(this[i]=="number")){
-            sum+=this[i]
-        }
-        
+//the add() method returns the sum of all elements which are Numbers.
+Array.prototype.add = function () {
+  let sum = 0;
+  for (let i = 0; i < this.length; i++) {
+    if (typeof (this[i] == "number")) {
+      sum += this[i];
     }
-    return sum;
-}
+  }
+  return sum;
+};
 console.log(arr.add());
 
-Array.prototype.break = function(start,end){
-    let arr = [];
-    if(end==null){
-        end=this.length;
+//---------------------------------------------------------------------//
+
+// The break() method returns a portion of an array into a new array
+// selected from start to end where start and end represent the index
+// of items in that array. The original array will not be modified.
+
+Array.prototype.break = function (start, end) {
+  let arr = [];
+  if (end == null) {
+    end = this.length;
+  }
+  for (let i = start; i <= end - start; i++) {
+    arr.push(this[i]);
+  }
+
+  return arr;
+};
+
+console.log(arr.break(1, 3));
+
+//--------------------------------------------------------------------//
+
+//The fillNum() method returns a new array which only contains Numbers.
+//The original array will not be modified
+
+Array.prototype.fillNum = function () {
+  let arr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (typeof this[i] != "number") {
+      continue;
     }
-    for(let i=start;i<=end-start;i++){
-        arr.push(this[i])
-    }
+    arr.push(this[i]);
+  }
+  return arr;
+};
 
-    return arr;
-}
-
-console.log(arr.break(1,3))
-
-Array.prototype.fillNum = function (){
-    let arr=[];
-
-    for(let i=0;i<this.length;i++){
-        if(typeof(this[i])!="number"){
-            continue;
-        }
-        arr.push(this[i])
-    }
-    return arr;
-
-}
-
-console.log(arr.fillNum())
+console.log(arr.fillNum());
